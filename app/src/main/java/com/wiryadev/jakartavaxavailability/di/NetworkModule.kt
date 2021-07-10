4f22d8297aef0f011b723b4519d114d5906bfe3c
wiryadev <abrarwiryawan@gmail.com>
2021-07-10 14:19:58 +0700

    Add NetworkModule.kt

---
+++ app/src/main/java/com/wiryadev/jakartavaxavailability/di/NetworkModule.kt
@@ -0,0 +1,34 @@
+package com.wiryadev.jakartavaxavailability.di
+
+import com.wiryadev.jakartavaxavailability.data.network.ApiService
+import com.wiryadev.jakartavaxavailability.data.network.Endpoint
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideService(httpClient: OkHttpClient): Endpoint {
+        return Retrofit
+            .Builder()
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(ApiService.BASE_URL)
+            .build()
+            .create(Endpoint::class.java)
+    }
+
+}
