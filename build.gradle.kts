0c023b284bdb4e518ef1b834fe5dd26f752cfc25
wiryadev <abrarwiryawan@gmail.com>
2021-07-12 10:19:33 +0700

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,24 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0-beta09")
+    val compose_version by extra("1.0.0-rc01")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta05")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
