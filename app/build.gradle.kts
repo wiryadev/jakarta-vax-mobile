c59dad339da4a16a27a34dafd0c9fa9588a718ef
wirya <abrarwiryawan@gmail.com>
2021-07-15 21:41:45 +0700

    enable minify and add proguard rules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,14 +24,14 @@ android {
 
     buildTypes {
         debug {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
